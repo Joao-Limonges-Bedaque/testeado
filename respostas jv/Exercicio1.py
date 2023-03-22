49e4894db0e73e5297ed3438bf3d3a1bf2bee7f3
Joao Vitor Bedaque <joaolimonges@gmail.com>
2023-03-22 09:03:16 -0400

    Adicionado uma forma de escolher a,b e c

---
+++ respostas jv/Exercicio1.py
@@ -0,0 +1,10 @@
+#Crie Uma fucção que recebe três numeros como parâmetro e retorne a soma deles
+
+def soma(a,b,c):
+    soma = a + b + c
+    return soma
+num1 = int(input('Digite um número para a soma :  '))
+num2 = int(input('Digite um número para a soma :  '))
+num3=int(input('Digite um número para a soma :  '))
+
+soma(num1,num2,num3)
