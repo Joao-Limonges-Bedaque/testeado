bea0266de996902a90b673905290cb3be20672da
Joao Vitor Bedaque <joaolimonges@gmail.com>
2023-03-22 09:31:08 -0400

    podendo criar sua lista para leitura.

--- respostas jv/Exercício3.py
+++ respostas jv/Exercício3.py
@@ -1,5 +1,11 @@
 #Crie uma função que recebe uma lista de números como parâmetro e retorne o maior numero da lista
 def maiorLista(a):
-    print(max(a))
-b = [16,42,5666,9999,1053]
+    print(f'O maior número digitado foi :   {max(a)}')
+b = []
+while True:
+    c = int(input('Digite m número para adicionar a lista ou digite 0 para terminar a lista \n:     '))
+    if c == 0 :
+        break
+    else:
+        b.append(c)
 maiorLista(b)
